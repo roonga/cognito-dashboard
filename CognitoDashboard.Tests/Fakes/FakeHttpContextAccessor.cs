1077615fa1f0f28134de45ba38e790b5ef156972
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-07-24 19:22:20 +1000

    Adding Test Data Generator

---
+++ CognitoDashboard.Tests/Fakes/FakeHttpContextAccessor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CognitoDashboard.Tests.Fakes
+{
+    internal class FakeHttpContextAccessor : IHttpContextAccessor
+    {
+        private HttpContext _httpContext = new DefaultHttpContext();
+
+        public HttpContext HttpContext { get => _httpContext; set => _httpContext = value; }
+    }
+}
