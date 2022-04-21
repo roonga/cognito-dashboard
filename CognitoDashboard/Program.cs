8f2385d9531d01aae351086f05f24e3ffde172a2
Ravi Mohan <ravi.mohan@roonga.com.au>
2022-04-22 07:50:56 +1000

    refactor : convert to .net 6 minimal api

--- CognitoDashboard/Program.cs
+++ CognitoDashboard/Program.cs
@@ -1,39 +1,101 @@
-using Serilog;
+using CognitoDashboard.Authorization;
+using CognitoDashboard.IdentityManager;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Serilog.Events;
+using Serilog;
 
-namespace CognitoDashboard;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(
+    new LoggerConfiguration()
+   .MinimumLevel.Information()
+   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+   .Enrich.FromLogContext()
+   .WriteTo.Console()
+   .CreateLogger());
+
+ConfigureOpenIdConnect(builder);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+
+builder.Services.AddSingleton<IAuthorizationHandler, DashboardAuthorizationHandler>();
+builder.Services.ConfigureIdentityManager(builder.Configuration);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
+
+void ConfigureOpenIdConnect(WebApplicationBuilder builder)
+{
+    var cognitoConfig = builder.Configuration.GetSection(CognitoConfig.Name).Get<CognitoConfig>();
+
+    builder.Services.AddAuthentication(options =>
     {
-        Log.Logger = new LoggerConfiguration()
-           .MinimumLevel.Information()
-           .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-           .Enrich.FromLogContext()
-           .WriteTo.Console()
-           .CreateLogger();
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.MetadataAddress = cognitoConfig.MetadataAddress;
+        options.ClientId = cognitoConfig.DashboardClientId;
+        options.ClientSecret = cognitoConfig.DashboardClientSecret;
+        options.SaveTokens = true;
 
-        try
+        options.Events.OnRedirectToIdentityProvider = async c =>
         {
-            Log.Information("Starting web host");
-            CreateHostBuilder(args).Build().Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Host terminated unexpectedly");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
+            c.ProtocolMessage.RedirectUri = cognitoConfig.RedirectUri;
+            await Task.CompletedTask;
+        };
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSerilog()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        options.Events.OnRedirectToIdentityProviderForSignOut = async c =>
+        {
+            c.ProtocolMessage.IssuerAddress = cognitoConfig.PostLogoutRedirectUri;
+            await Task.CompletedTask;
+        };
+
+        options.NonceCookie = new CookieBuilder
+        {
+            Name = OpenIdConnectDefaults.CookieNoncePrefix,
+            HttpOnly = true,
+            SameSite = SameSiteMode.None,
+            SecurePolicy = CookieSecurePolicy.Always,
+            IsEssential = true
+        };
+        options.CorrelationCookie = new CookieBuilder
+        {
+            SecurePolicy = CookieSecurePolicy.Always
+        };
+    });
+
+    // authorization for dashboard
+    builder.Services.AddAuthorization(options =>
+        options.AddPolicy(PolicyName.DashboardAdministrators, policy => policy.Requirements.Add(new DashboardAuthorizationRequirement("dashboard-administrators")))
+    );
 }
